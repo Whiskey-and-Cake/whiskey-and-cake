1b1963b047252166a0351e7dce0398a74a53c3cd
Sean Kim <skim2636@gmail.com>
2015-03-12 15:09:28 -0700

    (fix) fix conflicts in cardsagainstsobriety.html

--- client/cardsagainstsobriety.js
+++ client/cardsagainstsobriety.js
@@ -1,19 +1,15 @@
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  }),
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    user: function() {
+      return Meteor.user().username;
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
