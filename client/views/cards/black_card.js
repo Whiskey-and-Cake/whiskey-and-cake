10ce0e6754b63cae60bc7d1e137c811b361688f9
Johnnygames <johnegames@gmail.com>
2015-03-12 12:06:15 -0700

    Added views folder, cards folder and basic template and helper for black cards with some test data

---
+++ client/views/cards/black_card.js
@@ -0,0 +1,11 @@
+Template.blackCard.helpers({
+  cards: cardData
+  }
+});
+
+var cardData = [
+  {"id":642,"cardType":"Q","text":"When I pooped, what came out of my butt?","numAnswers":1,"expansion": "CAHe1"},
+  {"id":643,"cardType":"Q","text":"In the distant future, historians will agree that _ marked the beginning of America's decline.","numAnswers":1,"expansion": "CAHe1"},
+  {"id":644,"cardType":"Q","text":"In a pinch, _ can be a suitable substitute for _.","numAnswers":2,"expansion": "CAHe1"},
+  {"id":645,"cardType":"Q","text":"What has been making life difficult at the nudist colony?","numAnswers":1,"expansion": "CAHe1"}    
+]
