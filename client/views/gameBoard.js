b2c7b8731b99356a37b8f280a00af92659bb925a
dblanchard13 <davidb.prof@gmail.com>
2015-03-18 14:31:12 -0400

    Added some contextual and visual sugar Updated meteor to 1.0.4

--- client/views/gameBoard.js
+++ client/views/gameBoard.js
@@ -15,12 +15,15 @@ Template.gameBoard.helpers({
   },
 
   playas: function(){
+    //  this helper is loaded into the view as a way to see all the active  'cheaters', or, in other words, players.
+    //  it loops through all the users Meteor has and if it can't find any cheaters with that username, it adds them to cheaters.
+    //  the final return statement just provides all the cheaters to the gameboard view as 'playas'
+    //  Ideally this the loop through should happen elsewhere and this should just return all the cheaters since I think it's
+    //  the loop that causes users to get double added to cheaters.
     var users = Meteor.users.find({});
     
     users.forEach(function(user){
-      // console.log('user - ', user);
       if(Cheaters.findOne({username: user.username})){
-        // console.log('Tryna double cheat me...')
         return;
       } else {
         Cheaters.insert({username: user.username, score: 0})
@@ -55,6 +58,7 @@ Template.gameBoard.events({
   },
 
   "click .pickWinner": function(){
+    //  Whenever an in play card gets played, this checks for the corresponding 'cheater' and increases their score by 1
     var playedBy = this.playedBy;
 
     var cheat = Cheaters.findOne({username: playedBy});
