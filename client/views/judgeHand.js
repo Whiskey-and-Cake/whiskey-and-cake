1c7d68c610a5fbe530150aefa6536cd5d0d0d58d
Johnnygames <johnegames@gmail.com>
2015-03-20 15:09:20 -0700

    (feat) Add score incrementer to judgeHand, need to check if it works

--- client/views/judgeHand.js
+++ client/views/judgeHand.js
@@ -8,6 +8,7 @@ Template.judgeHand.helpers({
 Template.judgeHand.events({
   "click .card": function () {
     //increment score of card owner
+    Meteor.users.find({_id: this.owner}, {$set: {'score': 1}});
     //remove cards from GameBoard
     //pass 'judgeship' to next person
   }
